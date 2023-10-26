47c76e94515b12c2182e0d64949032257abebb60
Original-Nativy <dangiera11@gmail.com>
2023-10-26 22:32:29 +0700

    react project first

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    Input, Card, CardBody, CardHeader, ListGroup, Button,
+} from 'reactstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [
+        firstInteger,
+        setFirstInteger,
+    ] = React.useState<number | null >(0);
+    const [
+        secondInteger,
+        setSecondInteger,
+    ] = React.useState<number | null >(0);
+    const [
+        result,
+        setResult,
+    ] = React.useState<number | null >(0);
+
+    const russianMultiplying = (firstNumb: number | null, secondNumb: number | null) => {
+        if (firstNumb && secondNumb) {
+            let result = 0;
+            let firstNumbCopy = firstNumb;
+            let secondNumbCopy = secondNumb;
+            while (firstNumbCopy > 0) {
+                if (firstNumbCopy % 2 === 1) {
+                    result += secondNumbCopy;
+                }
+                firstNumbCopy = Math.floor(firstNumbCopy / 2);
+                secondNumbCopy *= 2;
+            }
+            return result;
+        }
+        return 0;
+    };
+
+    return (
+        <Card>
+            <CardHeader className='text-center'>Russian multiplying</CardHeader>
+            <CardBody className='p-0'>
+                <ListGroup className='mb-3 w-100 h-100'>
+
+                    <Input
+                        type="number"
+                        label="first Integer"
+                        className='m-3'
+                        value={firstInteger ?? ''}
+                        onChange={e => {
+                            setFirstInteger(e.target.valueAsNumber);
+                        }}
+                    />
+                    <Input
+                        type="number"
+                        label='second Integer'
+                        className='m-3'
+                        value={secondInteger ?? ''}
+                        onChange={e => {
+                            setSecondInteger(e.target.valueAsNumber);
+                        }}
+                    />
+                </ListGroup>
+                <ListGroup>
+                    <Button
+                        className='m-3 w-100'
+                        onClick={() => {
+                            setResult(russianMultiplying(firstInteger, secondInteger));
+                        }}
+                    />
+                    <Input
+                        type="number"
+                        readOnly
+                        className='m-3'
+                        value= {result ?? ''}
+                    />
+                </ListGroup>
+            </CardBody>
+        </Card>
+    );
 }
 
 export default App;
